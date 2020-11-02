ed5881f3508a19aadc1976ac07dad9f27f0b7d09
shahrour <ahmadshahrour951@gmail.com>
2020-11-02 14:19:23 -0500

    Annoying Prettier Formatting....

--- test_app.py
+++ test_app.py
@@ -1,22 +1,23 @@
 ################################################################################
-## IMPORTANT: DO NOT EDIT THIS CODE (unless you know what you're doing).
+# IMPORTANT: DO NOT EDIT THIS CODE (unless you know what you're doing).
 ##
-## To run the tests, first make sure you have pytest installed:
+# To run the tests, first make sure you have pytest installed:
 ##
-## $ pip3 install unittest
+# $ pip3 install unittest
 ##
-## Then, open up a terminal to the homework folder and run the following:
+# Then, open up a terminal to the homework folder and run the following:
 ##
-## $ python3 test_app.py
+# $ python3 test_app.py
 ##
-## If you want to run only a single test, you can run the following:
+# If you want to run only a single test, you can run the following:
 ##
-## $ python3 test_app.py TestApp.test_froyo
+# $ python3 test_app.py TestApp.test_froyo
 ################################################################################
 
 import unittest
 
 from app import app
+
 
 class TestApp(unittest.TestCase):
     """Unit tests for app.py."""
@@ -82,7 +83,8 @@ class TestApp(unittest.TestCase):
         self.assertIn('result is: 13', result_page_text)
 
     def test_calculator_subtract(self):
-        res = app.test_client().get('/calculator_results?operand1=6&operand2=7&operation=subtract')
+        res = app.test_client().get(
+            '/calculator_results?operand1=6&operand2=7&operation=subtract')
         self.assertEqual(res.status_code, 200)
 
         result_page_text = res.get_data(as_text=True)
@@ -90,7 +92,8 @@ class TestApp(unittest.TestCase):
         self.assertIn('result is: -1', result_page_text)
 
     def test_calculator_multiply(self):
-        res = app.test_client().get('/calculator_results?operand1=6&operand2=7&operation=multiply')
+        res = app.test_client().get(
+            '/calculator_results?operand1=6&operand2=7&operation=multiply')
         self.assertEqual(res.status_code, 200)
 
         result_page_text = res.get_data(as_text=True)
@@ -98,7 +101,8 @@ class TestApp(unittest.TestCase):
         self.assertIn('result is: 42', result_page_text)
 
     def test_calculator_divide(self):
-        res = app.test_client().get('/calculator_results?operand1=6&operand2=3&operation=divide')
+        res = app.test_client().get(
+            '/calculator_results?operand1=6&operand2=3&operation=divide')
         self.assertEqual(res.status_code, 200)
 
         result_page_text = res.get_data(as_text=True)
@@ -106,7 +110,8 @@ class TestApp(unittest.TestCase):
         self.assertIn('result is: 2', result_page_text)
 
     def test_compliments_3(self):
-        res = app.test_client().get('/compliments_results?users_name=Ducky&wants_compliments=yes&num_compliments=3')
+        res = app.test_client().get(
+            '/compliments_results?users_name=Ducky&wants_compliments=yes&num_compliments=3')
         self.assertEqual(res.status_code, 200)
 
         result_page_text = res.get_data(as_text=True)
@@ -114,7 +119,8 @@ class TestApp(unittest.TestCase):
         self.assertEqual(3, result_page_text.count('<li>'))
 
     def test_compliments_5(self):
-        res = app.test_client().get('/compliments_results?users_name=Moxie&wants_compliments=yes&num_compliments=5')
+        res = app.test_client().get(
+            '/compliments_results?users_name=Moxie&wants_compliments=yes&num_compliments=5')
         self.assertEqual(res.status_code, 200)
 
         result_page_text = res.get_data(as_text=True)
@@ -122,12 +128,14 @@ class TestApp(unittest.TestCase):
         self.assertEqual(5, result_page_text.count('<li>'))
 
     def test_compliments_none(self):
-        res = app.test_client().get('/compliments_results?users_name=Moxie&wants_compliments=no&num_compliments=3')
+        res = app.test_client().get(
+            '/compliments_results?users_name=Moxie&wants_compliments=no&num_compliments=3')
         self.assertEqual(res.status_code, 200)
 
         result_page_text = res.get_data(as_text=True)
         self.assertIn('Moxie', result_page_text)
         self.assertNotIn('<li>', result_page_text)
+
 
 if __name__ == '__main__':
     unittest.main()
