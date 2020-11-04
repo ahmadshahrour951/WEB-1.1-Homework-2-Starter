88f80f2e1495c1a9d5a7e3a793f746481016d117
shahrour <ahmadshahrour951@gmail.com>
2020-11-03 23:54:02 -0500

    Completed all challenges

--- app.py
+++ app.py
@@ -153,8 +153,16 @@ def compliments():
 @app.route('/compliments_results')
 def compliments_results():
     """Show the user some compliments."""
+
+    users_name = request.args.get('users_name')
+    wants_compliments = request.args.get('wants_compliments')
+    num_compliments = int(request.args.get('num_compliments'))
+    random_compliments = random.sample(list_of_compliments, num_compliments)
+
     context = {
-        # TODO: Enter your context variables here.
+        'users_name': users_name,
+        'wants_compliments': wants_compliments,
+        'random_compliments': random_compliments,
     }
 
     return render_template('compliments_results.html', **context)
